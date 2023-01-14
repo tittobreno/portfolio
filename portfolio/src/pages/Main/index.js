387742d18c2676ad3b9332a1079416fb584463c2
unknown <brenomoraes97@yahoo.com>
2023-01-14 03:28:26 -0300

    feat: create new sections to page main

--- portfolio/src/pages/Main/index.js
+++ portfolio/src/pages/Main/index.js
@@ -15,6 +15,22 @@ function Main() {
         <h3 className='about-txt primary'>Olá,</h3>
         <h3 className='about-txt secondary'>{`Eu sou o Breno!`}</h3>
         <span className='about-sub-txt'>Desenvolvedor Back-end</span>
+        <span className='about-sub-txt'>ABOUT-ME</span>
+      </section>
+
+      <section className='section'>
+        <h1 className='section-title'>HABILIDADES</h1>
+
+      </section>
+
+      <section className='section'>
+        <h1 className='section-title'>PROJETOS</h1>
+
+      </section>
+
+      <section className='section'>
+        <h1 className='section-title'>CONTATO</h1>
+
       </section>
     </main>
   );
