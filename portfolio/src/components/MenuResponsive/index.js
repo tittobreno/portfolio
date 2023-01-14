c7a4d8bc5709db13504ff594a1ef2150550752e0
unknown <brenomoraes97@yahoo.com>
2023-01-14 02:50:22 -0300

    feat: page responsive

---
+++ portfolio/src/components/MenuResponsive/index.js
@@ -0,0 +1,30 @@
+import './styles.css';
+import { useEffect, useRef } from 'react';
+import { useGlobal } from '../../contexts/GlobalContext';
+function MenuResponsive() {
+    const { openMenu } = useGlobal();
+    const overlayRef = useRef();
+
+    const handleMenu = () => {
+        if (openMenu) {
+            overlayRef.current.style.top = '10rem';
+        }
+    };
+
+    useEffect(() => {
+        handleMenu()
+    }, [])
+
+    return (
+        <main ref={overlayRef} className='overlay-menu-responsive'>
+
+            <h1 className='section-menu_item'>SOBRE</h1>
+            <h1 className='section-menu_item'>HABILIDADES</h1>
+            <h1 className='section-menu_item'>PROJETOS</h1>
+            <h1 className='section-menu_item'>CONTATO</h1>
+
+        </main>
+    );
+}
+
+export default MenuResponsive;
--- portfolio/src/pages/Main/index.js
+++ portfolio/src/pages/Main/index.js
@@ -1,11 +1,17 @@
 import './styles.css';
 import Header from '../../components/Header';
+import { useGlobal } from '../../contexts/GlobalContext';
+import MenuResponsive from '../../components/MenuResponsive';
 function Main() {
+  const { openMenu } = useGlobal();
   return (
+
     <main className='container-main'>
       <Header />
 
       <section className='container-about'>
+        {openMenu && <MenuResponsive />}
+
         <h3 className='about-txt primary'>Olá,</h3>
         <h3 className='about-txt secondary'>{`Eu sou o Breno!`}</h3>
         <span className='about-sub-txt'>Desenvolvedor Back-end</span>
