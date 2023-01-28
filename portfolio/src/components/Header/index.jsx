1a4c396c33fac0c88873816bfdf7182b73b46dae
unknown <brenomoraes97@yahoo.com>
2023-01-28 04:34:36 -0300

    refactor: icons menu responsive

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -1,28 +1,44 @@
-import './styles.css';
-import CondingIcon from '../../assets/coding.png';
-import MenuIconOpen from '../../assets/menu.png';
-import MenuIconClose from '../../assets/cancel.png';
-import { useGlobal } from '../../contexts/GlobalContext';
-
+import "./styles.css";
+import CondingIcon from "../../assets/coding.png";
+import MenuIconOpen from "../../assets/menu.png";
+import MenuIconClose from "../../assets/cancel.png";
+import { useGlobal } from "../../contexts/GlobalContext";
+import { FiMenu, FiX } from "react-icons/fi";
+import { motion } from "framer-motion";
 const Header = () => {
-    const { openMenu, setOpenMenu } = useGlobal()
-    return (
-        <header className='container-header'>
-            <img className='header-logo-icon' src={CondingIcon} alt='Logo' />
-            <section className='section-titles'>
-                <img
-                    className='section-titles_menu'
-                    src={openMenu ? MenuIconClose : MenuIconOpen}
-                    onClick={() => setOpenMenu(!openMenu)}
-                    alt='Menu'
-                />
-                <a className='section-titles_link' href='#about'><h1 className='section-titles_item'>SOBRE</h1></a>
-                <a className='section-titles_link' href='#skills'><h1 className='section-titles_item'>HABILIDADES</h1></a >
-                <a className='section-titles_link' href='#projects'><h1 className='section-titles_item'>PROJETOS</h1></a>
-                <a className='section-titles_link' href='#contact'><h1 className='section-titles_item'>CONTATO</h1></a>
-            </section>
-        </header>
-    )
+  const { openMenu, setOpenMenu } = useGlobal();
+
+  return (
+    <header className="container-header">
+      <img className="header-logo-icon" src={CondingIcon} alt="Logo" />
+      <section className="section-titles">
+        <motion.button
+          className="header-button_responsive"
+          onClick={() => setOpenMenu(!openMenu)}
+          whileTap={{ scale: 1.1 }}
+        >
+          {openMenu ? (
+            <FiX className="section-titles_menu" size={40} />
+          ) : (
+            <FiMenu className="section-titles_menu" size={40} />
+          )}
+        </motion.button>
+
+        <a className="section-titles_link" href="#about">
+          <h1 className="section-titles_item">SOBRE</h1>
+        </a>
+        <a className="section-titles_link" href="#skills">
+          <h1 className="section-titles_item">HABILIDADES</h1>
+        </a>
+        <a className="section-titles_link" href="#projects">
+          <h1 className="section-titles_item">PROJETOS</h1>
+        </a>
+        <a className="section-titles_link" href="#contact">
+          <h1 className="section-titles_item">CONTATO</h1>
+        </a>
+      </section>
+    </header>
+  );
 };
 
 export default Header;
--- portfolio/src/components/MenuResponsive/index.jsx
+++ portfolio/src/components/MenuResponsive/index.jsx
@@ -1,30 +1,57 @@
-import './styles.css';
-import { useEffect, useRef } from 'react';
-import { useGlobal } from '../../contexts/GlobalContext';
+import "./styles.css";
+import { useEffect, useRef } from "react";
+import { useGlobal } from "../../contexts/GlobalContext";
+import { motion } from "framer-motion";
 function MenuResponsive() {
-    const { openMenu, setOpenMenu } = useGlobal();
-    const overlayRef = useRef();
+  const { openMenu, setOpenMenu } = useGlobal();
+  const overlayRef = useRef();
 
-    const handleMenu = () => {
-        if (openMenu) {
-            overlayRef.current.style.top = '10rem';
-        }
-    };
+  const handleMenu = () => {
+    if (openMenu) {
+      overlayRef.current.style.top = "10rem";
+    }
+  };
 
-    useEffect(() => {
-        handleMenu()
-    })
+  useEffect(() => {
+    handleMenu();
+  });
 
-    return (
-        <main ref={overlayRef} className='overlay-menu-responsive'>
-
-            <a onClick={() => setOpenMenu(false)} href='#about' className='section-menu_item'>SOBRE</a>
-            <a onClick={() => setOpenMenu(false)} href='#skills' className='section-menu_item'>HABILIDADES</a>
-            <a onClick={() => setOpenMenu(false)} href='#projects' className='section-menu_item'>PROJETOS</a>
-            <a onClick={() => setOpenMenu(false)} href='#contact' className='section-menu_item'>CONTATO</a>
-
-        </main>
-    );
+  return (
+    <main ref={overlayRef} className="overlay-menu-responsive">
+      <motion.a
+        whileTap={{ scale: 1.1 }}
+        onClick={() => setOpenMenu(false)}
+        href="#about"
+        className="section-menu_item"
+      >
+        SOBRE
+      </motion.a>
+      <motion.a
+        whileTap={{ scale: 1.1 }}
+        onClick={() => setOpenMenu(false)}
+        href="#skills"
+        className="section-menu_item"
+      >
+        HABILIDADES
+      </motion.a>
+      <motion.a
+        whileTap={{ scale: 1.1 }}
+        onClick={() => setOpenMenu(false)}
+        href="#projects"
+        className="section-menu_item"
+      >
+        PROJETOS
+      </motion.a>
+      <motion.a
+        whileTap={{ scale: 1.1 }}
+        onClick={() => setOpenMenu(false)}
+        href="#contact"
+        className="section-menu_item"
+      >
+        CONTATO
+      </motion.a>
+    </main>
+  );
 }
 
 export default MenuResponsive;
