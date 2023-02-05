ca9be426bc59bf3ca0a61f28d4bf2f7869c574ad
unknown <brenomoraes97@yahoo.com>
2023-02-04 23:49:10 -0300

    style: reordering imports

--- portfolio/src/index.jsx
+++ portfolio/src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import MainRoutes from './routes';
-import { GlobalProvider } from './contexts/GlobalContext';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalProvider } from "./contexts/GlobalContext";
+import "./index.css";
+import MainRoutes from "./routes";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <GlobalProvider>
@@ -14,4 +14,3 @@ root.render(
     </GlobalProvider>
   </React.StrictMode>
 );
-
--- portfolio/src/utils/data.jsx
+++ portfolio/src/utils/data.jsx
@@ -1,17 +1,15 @@
-import JsIcon from "../assets/skills/js.svg";
-import PostgreIcon from "../assets/skills/postgresql.svg";
-import ReactIcon from "../assets/skills/react.svg";
-import HtmlIcon from "../assets/skills/html5.svg";
-import CssIcon from "../assets/skills/css3.svg";
-import GitIcon from "../assets/skills/git.svg";
 import EmailIcon from "../assets/contact/email.png";
+import GitHubIcon from "../assets/contact/github.png";
 import LinkedInIcon from "../assets/contact/linkedin.png";
 import PhoneIcon from "../assets/contact/phone.png";
-import GitHubIcon from "../assets/contact/github.png";
+import CssIcon from "../assets/skills/css3.svg";
+import GitIcon from "../assets/skills/git.svg";
+import HtmlIcon from "../assets/skills/html5.svg";
+import JsIcon from "../assets/skills/js.svg";
 import NodeIcon from "../assets/skills/nodejs.svg";
+import PostgreIcon from "../assets/skills/postgresql.svg";
+import ReactIcon from "../assets/skills/react.svg";
 import TypeIcon from "../assets/skills/ts.svg";
-import { RxLinkedinLogo } from "react-icons/rx";
-
 export const skills = [
   {
     name: "NODEJS",
